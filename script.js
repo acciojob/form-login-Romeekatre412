e6ef5878d32d65a562afd4039941fc9e784cfeac
Romee Katre <101556730+Romeekatre412@users.noreply.github.com>
2026-01-31 11:46:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
-    //Write your code here
-
+    event.preventDefault();
+    let firstName = document.forms["form1"]["fname"].value.trim();
+    let lastName = document.forms["form1"]["lname"].value.trim();
+    let fullName = firstName + " " + lastName;
+	
+    alert(fullName);
 }
